2b1ffc16a316ff4ecf7f17c835e6b55650686031
noriksaroyan <noriksaroyan@gmail.com>
2021-05-20 00:01:51 +0300

    add apple

---
+++ src/GeekBrains/Apple.java
@@ -0,0 +1,16 @@
+package GeekBrains;
+
+public class Apple extends Fruit{
+
+    @Override
+    public float getWeight() {
+        return Weight;
+    }
+
+    public void setWeight(Float weight) {
+        Weight = weight;
+    }
+
+    public float Weight = 1.0f;
+
+}
